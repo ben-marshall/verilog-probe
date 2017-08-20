9d7cc59ceaf3f030ba44a766b98ac64c97fff116
Ben Marshall <bm12656@my.bristol.ac.uk>
2017-08-20 12:35:13 +0100

    Start expanding the testbench.  On branch master  Your branch is ahead of 'origin/master' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   src/test/tb.v

--- src/test/tb.v
+++ src/test/tb.v
@@ -9,6 +9,35 @@
 module tb_uartprobe ();
 
 //
+// Commands for the probe.
+//
+
+localparam [7:0] CMD_GPI_RD0    = 8'd2;
+localparam [7:0] CMD_GPI_RD1    = 8'd3;
+localparam [7:0] CMD_GPI_RD2    = 8'd4;
+localparam [7:0] CMD_GPI_RD3    = 8'd5;
+localparam [7:0] CMD_GPO_RD0    = 8'd6;
+localparam [7:0] CMD_GPO_RD1    = 8'd7;
+localparam [7:0] CMD_GPO_RD2    = 8'd8;
+localparam [7:0] CMD_GPO_RD3    = 8'd9;
+localparam [7:0] CMD_GPO_WR0    = 8'd10;
+localparam [7:0] CMD_GPO_WR1    = 8'd11;
+localparam [7:0] CMD_GPO_WR2    = 8'd12;
+localparam [7:0] CMD_GPO_WR3    = 8'd13;
+localparam [7:0] CMD_AXI_RD0    = 8'd14;
+localparam [7:0] CMD_AXI_RD1    = 8'd15;
+localparam [7:0] CMD_AXI_RD2    = 8'd16;
+localparam [7:0] CMD_AXI_RD3    = 8'd17;
+localparam [7:0] CMD_AXI_WR0    = 8'd18;
+localparam [7:0] CMD_AXI_WR1    = 8'd19;
+localparam [7:0] CMD_AXI_WR2    = 8'd20;
+localparam [7:0] CMD_AXI_WR3    = 8'd21;
+localparam [7:0] CMD_AXI_RD     = 8'd22;
+localparam [7:0] CMD_AXI_WR     = 8'd23;
+localparam [7:0] CMD_AXI_RDC    = 8'd24;
+localparam [7:0] CMD_AXI_WRC    = 8'd25;
+
+//
 // DUT I/O signals
 //
 
@@ -59,7 +88,11 @@ integer clock_counter;
 // Simple testbench control
 //
 
-always #5 clk = !clk;
+localparam CLOCK_PERIOD = 5;
+always #(CLOCK_PERIOD) clk = !clk;
+
+initial rx_valid = 1'b0;
+initial tx_ready = 1'b0;
 
 //
 // Wave dumping and reset.
@@ -85,6 +118,9 @@ always @(posedge clk) begin
         $finish;
     end
 end
+
+
+// ---------------- AXI Handling ---------------------------------------
     
 
 //
@@ -113,6 +149,8 @@ task handle_reads;
     end
 endtask
 
+
+
 //
 // Start all the different channel handlers off on separate threads.
 //
@@ -128,6 +166,245 @@ initial begin
     fork
         handle_reads();
     join
+end
+
+// ---------------- Command IO -----------------------------------------
+
+//  
+//  task: send_byte
+//
+//      Sends a single byte down the UART RX line to the DUT.
+//
+task send_byte;
+    input [7:0] to_send;
+    integer i;
+    begin
+        $display("UART TX> Sending byte: %h,\t %d,\t %b at time %d",
+            to_send,to_send,to_send, $time);
+
+        rx_data = to_send;
+        rx_valid = 1'b1;
+        @(posedge clk);
+        wait(rx_ready);
+        @(posedge clk);
+        rx_valid = 1'b0;
+
+    end
+endtask
+
+//  
+//  task: recieve_byte
+//
+//      Recieve a single byte from the UART TX line from the DUT.
+//
+task recieve_byte;
+    output [7:0] recieved;
+    input  [0:0] quiet;
+    integer i;
+    begin
+        @(posedge clk);
+        wait(tx_valid);
+        @(posedge clk);
+        recieved = tx_data;
+        tx_ready = 1'b1;
+        @(posedge clk);
+        wait(!tx_valid);
+        tx_ready = 1'b0;
+        if(!quiet) begin
+            $display("UART RX> Recieved byte: %h,\t %d,\t %b at time %d",
+                recieved,recieved,recieved, $time);
+        end
+    end
+endtask
+
+//
+//  task: expect_byte
+//
+//      Wrapper around the recieve_byte task which checks if the expected
+//      byte value was recieved.
+//
+task expect_byte;
+    input [7:0] expected_value;
+    reg   [7:0] recieved_value;
+    begin
+        recieve_byte(recieved_value, 1'b1);
+        if(expected_value != recieved_value) begin
+            $display("[ERROR] Expected to recieve %h, got %h",
+                expected_value, recieved_value);
+        end
+    end
+endtask
+
+
+//
+//  task: read_gpi
+//
+//      Read the supplied GPI byte and have it returned via the UART TX line.
+//  
+task read_gpi;
+    input [1:0] gpi;
+    begin
+        case(gpi)
+            0   : send_byte(CMD_GPI_RD0);
+            1   : send_byte(CMD_GPI_RD1);
+            2   : send_byte(CMD_GPI_RD2);
+            3   : send_byte(CMD_GPI_RD3);
+        endcase
+    end
+endtask
+
+
+//
+//  task: read_gpo
+//
+//      Read the supplied GPO byte and have it returned via the UART TX line.
+//  
+task read_gpo;
+    input [1:0] gpo;
+    begin
+        case(gpo)
+            0   : send_byte(CMD_GPO_RD0);
+            1   : send_byte(CMD_GPO_RD1);
+            2   : send_byte(CMD_GPO_RD2);
+            3   : send_byte(CMD_GPO_RD3);
+        endcase
+    end
+endtask
+
+
+//
+//  task: write_gpo
+//
+//      Write the supplied GPO byte with a given value.
+//  
+task write_gpo;
+    input [1:0] gpo;
+    input [7:0] val;
+    begin
+        case(gpo)
+            0   : send_byte(CMD_GPO_WR0);
+            1   : send_byte(CMD_GPO_WR1);
+            2   : send_byte(CMD_GPO_WR2);
+            3   : send_byte(CMD_GPO_WR3);
+        endcase
+        send_byte(val);
+    end
+endtask
+
+
+//
+//  task: read_axi_addr
+//
+//      Read a byte of the AXI interface address.
+//  
+task read_axi_addr;
+    input [1:0] byte;
+    begin
+        case(byte)
+            0   : send_byte(CMD_AXI_RD0);
+            1   : send_byte(CMD_AXI_RD1);
+            2   : send_byte(CMD_AXI_RD2);
+            3   : send_byte(CMD_AXI_RD3);
+        endcase
+    end
+endtask
+
+
+//
+//  task: write_axi_addr
+//
+//      Write a byte of the AXI interface address.
+//  
+task write_axi_addr;
+    input [1:0] byte;
+    input [7:0] value;
+    begin
+        case(byte)
+            0   : send_byte(CMD_AXI_WR0);
+            1   : send_byte(CMD_AXI_WR1);
+            2   : send_byte(CMD_AXI_WR2);
+            3   : send_byte(CMD_AXI_WR3);
+        endcase
+        send_byte(value);
+    end
+endtask
+
+//
+//  task: read_axi_ctrl
+//
+//      Read the AXI control register.
+//  
+task read_axi_ctrl;
+    send_byte(CMD_AXI_RDC);
+endtask
+
+//
+//  task: write_axi_ctrl 
+//
+//      Write a value to the AXI control register.
+//  
+task write_axi_ctrl;
+    send_byte(CMD_AXI_WRC);
+endtask
+
+
+//
+//  task: read_axi_data
+//
+//      Read the last returned value in the AXI data register.
+//  
+task read_axi_data;
+    send_byte(CMD_AXI_RD);
+endtask
+
+
+//
+//  task: write_axi_data
+//
+//      Perform an AXI interface write to the current address in the AXI
+//      address register of the supplied value.
+//  
+task write_axi_data;
+    input [7:0] value;
+    begin
+        send_byte(CMD_AXI_WR);
+        send_byte(value);
+    end
+endtask
+
+// ---------------- Test Sequences -------------------------------------
+
+//
+// The main simple test sequence
+//
+initial begin : main_test_sequence
+    integer i;
+
+    #(CLOCK_PERIOD*40);
+
+    // Some reads.
+    read_gpo(0);
+    write_gpo(0,8'hAB);
+    fork
+    read_gpo(0);
+    join
+    expect_byte(8'hAB);
+    
+    #(CLOCK_PERIOD*4);
+    
+    $finish;
+
+end
+
+
+//
+// Monitor sequence for the UART TX line.
+//
+initial begin : uart_tx_monitor
+    forever begin : uart_tx_monitor_loop
+        reg [7:0] recieved;
+        recieve_byte(recieved,1'b0);
+    end
 end
 
 
